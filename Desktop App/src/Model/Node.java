343aee918c896138a2311a4b435648fdf9376068
FRizZL <jeffrey@vdleije.nl>
2012-09-06 12:18:38 +0200

    Node toegevoegd

---
+++ Desktop App/src/Model/Node.java
@@ -0,0 +1,14 @@
+package Model;
+
+/**
+ * The node is a spot which can be placed on a map. You can say that a spot represents a location.
+ */
+public class Node {
+    
+    // Variables
+    private int id;
+    private double longitude;
+    private double latitude;
+    
+    
+}
