d1b47ccd98006fc6cb43d76c49d40a85ec04f110
Tklaversma <Tklaversma@gmail.com>
2012-09-06 11:17:12 +0200

    Aanmaken Singleton Controller

---
+++ Desktop App/src/Controller/Controller.java
@@ -0,0 +1,35 @@
+package Controller;
+
+/**
+ * The controller mediates input and converting it to commands for the Model or View
+ */
+public class Controller {
+
+    // Variables
+    private static Controller controller = null;
+
+    /**
+     * Overload the public constructor
+     */
+    private Controller() {
+    }
+
+    /**
+     * A synchronized creator to prevent multithreading problems
+     */
+    private synchronized static void createInstance() {
+        if (controller == null) {
+            controller = new Controller();
+        }
+    }
+
+    /**
+     * Get the one and only Controller
+     */
+    public static Controller getInstance() {
+        if (controller == null) {
+            createInstance();
+        }
+        return controller;
+    }
+}
