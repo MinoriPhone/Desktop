9e6e93cdfbe8b6077a71c61118d04b553976c43e
Tklaversma <Tklaversma@gmail.com>
2012-09-06 11:54:29 +0200

    back to JDK 6

---
+++ Desktop App/src/Model/Route.java
@@ -0,0 +1,8 @@
+package Model;
+
+/**
+ * The Route an user is able to make during the Story. A Route holds all the Links between different Nodes (positions).
+ */
+public class Route {
+    
+}
---
+++ Desktop App/src/Model/Story.java
@@ -0,0 +1,10 @@
+package Model;
+
+/**
+ * A Story that the user is going to watch. This class holds all the different Routes an user can make.
+ */
+public class Story {
+    
+    // Variables
+    
+}
